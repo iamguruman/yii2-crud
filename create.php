812a235b5422a5ca3f6c9c2eaf998b37f7d470aa
Alex <39405237+iamguruman@users.noreply.github.com>
2021-06-09 19:35:31 +0300

    Create create.php

---
+++ create.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\jps\models\MJpsVcard */
+
+$this->title = 'Связать Юрлицо и Визитку';
+
+$this->params['breadcrumbs'][] = ['label' => 'Список юридических лиц', 'url' => ['/jps']];
+
+$this->params['breadcrumbs'][] = ['label' => 'Связь юрлиц и визиток', 'url' => ['/jps/vcard']];
+
+if($model->jp){
+    $this->params['breadcrumbs'][] = $model->jp->getBreadcrumbs();
+}
+
+if($model->vcard){
+    $this->params['breadcrumbs'][] = $model->vcard->getBreadcrumbs();
+}
+
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="mjps-vcard-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
