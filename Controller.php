e0cf4c301ede1553b3bfdb464d026baf3992b25b
Alex <39405237+iamguruman@users.noreply.github.com>
2021-09-14 19:31:50 +0300

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -99,7 +99,7 @@ class DefaultController extends Controller
     {
         $model = $this->findModel($id);
 
-        return aControllerActionMarkdel($this, $model, $model->urlView, $model->urlIndex);
+        return aControllerActionMarkdel($this, $model, $model->getUrlView(), $model->getUrlIndex());
 
         return $this->redirect(['index']);
     }
